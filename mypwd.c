8f4e9b4e65d986f77081aaf5b05105bba71fcd4c
Jacknlbq <2979734778@qq.com>
2024-10-25 18:29:18 +0800

    Add files via upload

--- mypwd.c
+++ mypwd.c
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 
 
-void printf_work_directory(const char *current_path,struct stat *statbuf)
+void printf_work_directory(const char *current_path,struct stat statbuf)
 {
      DIR *dir;
      struct dirent *entry;
@@ -18,15 +18,20 @@ void printf_work_directory(const char *current_path,struct stat *statbuf)
     char full_path[1024];
       while ((entry = readdir(dir)) != NULL) {  
           
-        if(entry->d_ino==statbuf->st_ino){
+        if(entry->d_ino==statbuf.st_ino){
             snprintf(full_path, sizeof(full_path), "/%s", entry->d_name); 
            break;
         }
       }
-    struct stat *statbuf_above;
-    stat("")
+    struct stat statbuf_above;
+    if (stat(current_path, &statbuf_above) == -1) {  
+            perror("stat");   
+        }
+    char above_path[100];
+    snprintf(above_path, sizeof(full_path), "../%s", current_path);
     //自调用，向上找
-    printf_work_directory("..",*statbuf_above);
+    if(statbuf_above.st_ino!='/')
+    printf_work_directory(above_path,statbuf_above);
 
    printf("%s",full_path);
 
@@ -40,6 +45,6 @@ int main()
     if (stat(".", &statbuf) == -1) {  
             perror("stat");   
         }
-        printf_work_directory()
+        printf_work_directory(".",statbuf);
      return 0;
 }
